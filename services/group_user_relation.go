956aefb4ff7ce2749d065a4b4dd42a52dde609f1
liujiaxing <liujiaxing@tenxcloud.com>
2018-12-28 13:11:34 +0800

    group user relations 实现缓存功能

--- services/group_user_relation.go
+++ services/group_user_relation.go
@@ -1,6 +1,7 @@
 package services
 
 import (
+	"fmt"
 	"github.com/jinzhu/gorm"
 	log "gopkg.in/cihub/seelog.v2"
 	"message_notification_practice"
@@ -19,40 +20,86 @@ type GroupUserRelationService struct {
 	cache redis.Cache
 }
 
-func (u *GroupUserRelationService) Create(gur []root.GroupUserRelation) error {
-	tx := u.db.Begin()
+func (svc *GroupUserRelationService) Create(gur []root.GroupUserRelation) error {
+	var err error
+	tx := svc.db.Begin()
 
 	for _, one := range gur {
 		log.Debugf("INSERT: %v\n", one)
-		if err := u.db.Create(&one).Error; err != nil {
-			tx.Rollback()
-			return err
+
+		cacheKey := svc.getGroupUsersCacheKey(one.GroupID)
+
+		err = svc.db.Create(&one).Error
+		if err != nil {
+			log.Error("group user relations create failed, err: ", err)
+			svc.cache.Delete(cacheKey)
+			break
+		}
+		user := root.User{ID: 1}
+
+		if err = svc.db.Find(&user).Error; err != nil {
+			log.Error("group user find user failed, err: ", err)
+			svc.cache.Delete(cacheKey)
+			break
+		}
+
+		if err := svc.cache.SAdd(cacheKey, &user); err != nil {
+			log.Errorf("group user SAdd user failed, cacheKey: %s, err: %s", cacheKey, err)
+			svc.cache.Delete(cacheKey)
 		}
 	}
 
-	tx.Commit()
+	if err != nil {
+		tx.Rollback()
+	} else {
+		tx.Commit()
+	}
 
+	return err
+}
+
+func (svc *GroupUserRelationService) Update(gur *root.GroupUserRelation, fields map[string]interface{}) error {
+	panic("not implemented")
+	svc.cache.Delete(svc.getGroupUsersCacheKey(gur.GroupID))
 	return nil
 }
 
-func (u *GroupUserRelationService) Update(gur *root.GroupUserRelation, fields map[string]interface{}) error {
-	panic("not implemented")
-}
-
-func (u *GroupUserRelationService) FindMembers(id uint64) ([]root.User, error) {
+func (svc *GroupUserRelationService) FindMembers(id uint64) ([]root.User, error) {
 	var users []root.User
 	gur := root.GroupUserRelation{GroupID: id}
 
-	err := u.db.Where("id in (?)", u.db.Model(&gur).Where(gur).Select("user_id").QueryExpr()).Find(&users).Error
+	cacheKey := svc.getGroupUsersCacheKey(gur.GroupID)
+	if svc.cache.IsExist(cacheKey) {
+		if err := svc.cache.SMembers(cacheKey, &users); err != nil {
+			svc.cache.Delete(cacheKey)
+			log.Errorf("FindMembers cache smembers failed, group_id: %d, err: %s", id, err)
+		} else {
+			log.Debugf("Find Members from cahce, group_id: %d\n", gur.GroupID)
+			return users, nil
+		}
+	}
+
+	err := svc.db.Where("id in (?)", svc.db.Model(&gur).Where(gur).Select("user_id").QueryExpr()).Find(&users).Error
 	//err := u.db.Raw("select * from users where id in (select user_id from group_user_relations where group_id = ?)", id).Scan(&users).Error
 	if err != nil {
 		log.Errorf("get group(%d) members failed, err: %s\n", id, err)
 	}
 
+	s := make([]interface{}, len(users))
+	for i, v := range users {
+		s[i] = v
+	}
+
+	if err := svc.cache.SAdd(cacheKey, s...); err != nil {
+		log.Errorf("FindMembers cache SAdd failed, group_id: %d, err: %s", id, err)
+	}
+
+	log.Debugf("Find Members from mysql, group_id: %d\n", gur.GroupID)
+
 	return users, err
 }
 
-func (u *GroupUserRelationService) FindAvailableMembers(id uint64, uname string) ([]root.User, error) {
+func (svc *GroupUserRelationService) FindAvailableMembers(id uint64, uname string) ([]root.User, error) {
 	var users []root.User
 	gur := root.GroupUserRelation{GroupID: id}
 
@@ -61,8 +108,8 @@ func (u *GroupUserRelationService) FindAvailableMembers(id uint64, uname string)
 	//	"%"+uname).QueryExpr()
 	//log.Debug("FindAvailableMembers: expr: %#v\n", expr)
 
-	err := u.db.Where("id not in (?) and name like ?",
-		u.db.Model(&gur).Where(gur).Select("user_id").QueryExpr(),
+	err := svc.db.Where("id not in (?) and name like ?",
+		svc.db.Model(&gur).Where(gur).Select("user_id").QueryExpr(),
 		uname+"%").Find(&users).Error
 	//err := u.db.Where ("name LIKE ?", "%sh%").Find(&users).Error
 
@@ -73,17 +120,20 @@ func (u *GroupUserRelationService) FindAvailableMembers(id uint64, uname string)
 	return users, err
 }
 
-func (u *GroupUserRelationService) FindByName(name string) (*root.GroupUserRelation, error) {
+func (svc *GroupUserRelationService) FindByName(name string) (*root.GroupUserRelation, error) {
 	panic("not implemented")
 }
 
-func (u *GroupUserRelationService) Delete(gur []root.GroupUserRelation) error {
+func (svc *GroupUserRelationService) Delete(gur []root.GroupUserRelation) error {
 
-	tx := u.db.Begin()
+	tx := svc.db.Begin()
 
 	for _, one := range gur {
 		log.Debugf("DELETE: %v\n", one)
-		if err := u.db.Where(one).Delete(root.GroupUserRelation{}).Error; err != nil {
+
+		svc.cache.Delete(svc.getGroupUsersCacheKey(one.GroupID))
+
+		if err := svc.db.Where(one).Delete(root.GroupUserRelation{}).Error; err != nil {
 			tx.Rollback()
 			return err
 		}
@@ -92,4 +142,8 @@ func (u *GroupUserRelationService) Delete(gur []root.GroupUserRelation) error {
 	tx.Commit()
 
 	return nil
+}
+
+func (svc *GroupUserRelationService) getGroupUsersCacheKey(groupId uint64) string {
+	return fmt.Sprintf("group_user_relations_group_users_%d", groupId)
 }
